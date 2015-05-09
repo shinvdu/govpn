38ea2939829ac622f5d1b216848c4fcdcd3a8e92
Sergey Matveev <stargrave@stargrave.org>
2015-05-10 00:38:10 +0300

    Include dependent libraries source code in release tarballs
    Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- utils/makedist.sh
+++ utils/makedist.sh
@@ -4,20 +4,40 @@ cur=$(pwd)
 tmp=$(mktemp -d)
 release=$1
 [ -n "$release" ]
+
 git clone . $tmp/govpn-$release
+cat > $tmp/includes <<EOF
+github.com
+golang.org/x/crypto/AUTHORS
+golang.org/x/crypto/CONTRIBUTORS
+golang.org/x/crypto/LICENSE
+golang.org/x/crypto/PATENTS
+golang.org/x/crypto/README
+golang.org/x/crypto/curve25519
+golang.org/x/crypto/pbkdf2
+golang.org/x/crypto/poly1305
+golang.org/x/crypto/salsa20
+golang.org/x/crypto/xtea
+EOF
+tar cfCI - src $tmp/includes | tar xfC - $tmp/govpn-$release/src
+rm $tmp/includes
+
 cd $tmp/govpn-$release
 git checkout $release
-rm -fr .git
-find . -name .gitignore -delete
+
 cat > doc/download.texi <<EOF
 @node Prepared tarballs
 @section Prepared tarballs
 You can obtain releases source code prepared tarballs on
 @url{http://www.cypherpunks.ru/govpn/}.
 EOF
-rm utils/makedist.sh
 make -C doc
-cd $tmp
+
+rm utils/makedist.sh
+find . -name .git -type d | xargs rm -fr
+find . -name .gitignore -delete
+
+cd ..
 tar cvf govpn-"$release".tar govpn-"$release"
 xz -9 govpn-"$release".tar
 gpg --detach-sign --sign --local-user FFE2F4A1 govpn-"$release".tar.xz
