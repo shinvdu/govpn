cd710ad3f9276699c7eaa72b956efa9bed705217
Sergey Matveev <stargrave@stargrave.org>
2015-08-23 17:42:09 +0300

    Refactor makedist.sh to make it aware of submodules
    Signed-off-by: Sergey Matveev <stargrave@stargrave.org>

--- utils/makedist.sh
+++ utils/makedist.sh
@@ -6,8 +6,14 @@ release=$1
 [ -n "$release" ]
 
 git clone . $tmp/govpn-$release
+for repo in src/github.com/bigeagle/water src/github.com/agl/ed25519 src/golang.org/x/crypto; do
+    git clone $repo $tmp/govpn-$release/$repo
+done
+cd $tmp/govpn-$release
+git checkout $release
+git submodule update --init
+
 cat > $tmp/includes <<EOF
-github.com
 golang.org/x/crypto/AUTHORS
 golang.org/x/crypto/CONTRIBUTORS
 golang.org/x/crypto/LICENSE
@@ -19,11 +25,10 @@ golang.org/x/crypto/poly1305
 golang.org/x/crypto/salsa20
 golang.org/x/crypto/xtea
 EOF
-tar cfCI - src $tmp/includes | tar xfC - $tmp/govpn-$release/src
-rm $tmp/includes
-
-cd $tmp/govpn-$release
-git checkout $release
+tar cfCI - src $tmp/includes | tar xfC - $tmp
+rm -fr src/golang.org
+mv $tmp/golang.org src/
+rm -fr $tmp/golang.org $tmp/includes
 
 cat > doc/download.texi <<EOF
 @node Prepared tarballs
@@ -40,5 +45,5 @@ find . -name .gitignore -delete
 cd ..
 tar cvf govpn-"$release".tar govpn-"$release"
 xz -9 govpn-"$release".tar
-gpg --detach-sign --sign --local-user FFE2F4A1 govpn-"$release".tar.xz
+gpg --detach-sign --sign --local-user F2F59045FFE2F4A1 govpn-"$release".tar.xz
 mv $tmp/govpn-"$release".tar.xz $tmp/govpn-"$release".tar.xz.sig $cur/doc/govpn.html/download
